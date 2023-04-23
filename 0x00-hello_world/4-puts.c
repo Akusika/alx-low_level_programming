43f3be36df29df5bd46f508649ec37209c722be9
Akusika <akusikabuatsi96@gmail.com>
2023-04-23 11:19:08 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#include < stdio.h>
- /**
-  * main -Entry point
-  *
-  * Return: Always 0 (Success)
-  */
+#include <stdio.h>
+/**
+*main - main entry point
+*return: Always 0 (Success)
+*/
 int main(void)
-{	
-printf("size of a char: %d byte(s)",sizeof(char));
-printf("size of an int: %d byte(s)",sizeof(int));
-printf("size of a long int:%d byte(s)",sizeof(long int));
-printf("size of of a long long int:%d byte(s)",sizeof(long long int));
-printf("size of a float:%d byte(s)",sizeof(float));
-return (0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
