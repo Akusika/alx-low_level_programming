f12e857493565bfe3205aef90abdee1e070e95ed
Akusika <akusikabuatsi96@gmail.com>
2023-05-06 00:00:53 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  * Description - prints a quote on a new line
  * Return - always 1 (success)
  */
-int main(void
-)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
