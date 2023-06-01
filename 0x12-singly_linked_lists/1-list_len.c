cfa8b769cd444fd183a107f1498e6c0426e0b556
Akusika <akusikabuatsi96@gmail.com>
2023-06-01 15:38:37 -0700

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,19 +3,19 @@
 
 /**
  * list_len - a function that returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * @h: pointer to the linked list
  *
  * Return: number of elements in h
  */
 
 size_t list_len(const list_t *h)
 {
- size_t j = 0;
+	size_t j = 0;
 
- while (h)
- {
- j++;
- h = h->next;
- }
- return (j);
+	while (h)
+	{
+		j++;
+		h = h->next;
+	}
+	return (j);
 }
