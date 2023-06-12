6255f7d89fda2bbd0f779df5fa0006e4b509d40a
Akusika <akusikabuatsi96@gmail.com>
2023-06-12 16:09:49 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
   * Return: 1 if successful, -1 if file cannot be written
   */
 
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
